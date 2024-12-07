36167c2d8520309153ad444213bada0bde5e82ff
Stefan Bogdanel <bogdan.izdrail@gmail.com>
2024-12-07 11:30:54 +0000

    Added release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the current week number
+current_week=$(date +%V)
+
+# Create the version string
+version="0.$current_week"
+
+echo "Creating and pushing tag: $version"
+
+git tag -a $version -m "Release $version"
+
+git push origin $version
